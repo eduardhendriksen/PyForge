d35f208d61624ae4e783568bd6a46c63f3b7aab0
eduardhendriksen <e.c.hendriksen@gmail.com>
2020-07-30 10:53:02 +0200

    updated docstring

--- PyForge/ForgePermissionGet.py
+++ PyForge/ForgePermissionGet.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Module containing classes related to folders on the Autodesk Forge BIM360 platform."""
+"""Module containing classes related to permissions on the Autodesk Forge BIM360 platform."""
 import re
 from PyForge.ForgeApi import ForgeApi
 
